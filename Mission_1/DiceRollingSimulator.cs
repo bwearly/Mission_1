00fe07671473b33eec363450b07b90d8c3b2bdd0
bwearly <153124661+bwearly@users.noreply.github.com>
2025-01-15 09:14:18 -0700

    Added comments

--- Mission_1/DiceRollingSimulator.cs
+++ Mission_1/DiceRollingSimulator.cs
@@ -13,24 +13,30 @@ namespace DiceRollingSimulator
             System.Console.WriteLine("Welcome to the dice throwing simulator!");
             System.Console.Write("How many dice rolls would you like to simulate? ");
 
+            // Collects the user input for the number of rolls
             string userInput = System.Console.ReadLine() ?? "";
 
+            // Checks if input is a valid number
             if (int.TryParse(userInput, out int numberOfRolls) && numberOfRolls > 0)
             {
                 System.Console.WriteLine($"\nYou chose to simulate {numberOfRolls} dice rolls.");
 
+                // Passes the number of rolls to the other class
                 int[] rollResults = RollClass.SimulateRolls(numberOfRolls);
 
+                // Prints the results of the dice rolls
                 System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
                 System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
                 System.Console.WriteLine($"Total number of rolls = {numberOfRolls}.\n");
 
+                // Prints the histogram with the numbers associated with them
                 for (int diceSum = 2; diceSum <= 12; diceSum++)
                 {
                     int percentage = (int)Math.Round((double)rollResults[diceSum] / numberOfRolls * 100);
                     System.Console.WriteLine($"{diceSum}: {new string('*', percentage)}");
                 }
             }
+            // If the input is not valid, this will run
             else
             {
                 System.Console.WriteLine("Invalid input. Please enter a positive whole number.");
--- Mission_1/SimulateRollClass.cs
+++ Mission_1/SimulateRollClass.cs
@@ -10,10 +10,13 @@ namespace SimulateRollClass
     {
         public static int[] SimulateRolls(int numDiceRolls)
         {
+            // Creates an integer array to house the number of rolls
             int[] rollsCount = new int[13];
 
+            // Pulls in the random number generatator class
             Random random = new Random();
 
+            // Based on the number of rolls, the dice will roll which will collect the results in an array
             for (int i = 0; i < numDiceRolls; i++)
             {
                 int dice1 = random.Next(1, 7);
@@ -22,7 +25,7 @@ namespace SimulateRollClass
 
                 rollsCount[diceSum]++;
             }
-
+            // Array is passed to the other class
             return rollsCount;
         }
     }
