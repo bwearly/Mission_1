61cc9d9a1c0202852593e6ca30775871b4324221
bwearly <153124661+bwearly@users.noreply.github.com>
2025-01-15 09:06:50 -0700

    added a second class

--- Mission_1/DiceRollingSimulator.cs
+++ Mission_1/DiceRollingSimulator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using SimulateRollClass;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DiceRollingSimulator
 
@@ -10,13 +13,13 @@ namespace DiceRollingSimulator
             System.Console.WriteLine("Welcome to the dice throwing simulator!");
             System.Console.Write("How many dice rolls would you like to simulate? ");
 
-            string userInput = System.Console.ReadLine();
+            string userInput = System.Console.ReadLine() ?? "";
 
             if (int.TryParse(userInput, out int numberOfRolls) && numberOfRolls > 0)
             {
                 System.Console.WriteLine($"\nYou chose to simulate {numberOfRolls} dice rolls.");
 
-                int[] rollResults = DiceRollingSimulator.SimulateRolls(numberOfRolls);
+                int[] rollResults = RollClass.SimulateRolls(numberOfRolls);
 
                 System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
                 System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
@@ -34,25 +37,6 @@ namespace DiceRollingSimulator
             }
             System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
         }
-
-        public static int[] SimulateRolls(int numDiceRolls)
-        {
-            int[] rollsCount = new int[13];
-
-            Random random = new Random();
-
-            for (int i = 0; i < numDiceRolls; i++)
-            {
-                int dice1 = random.Next(1,7);
-                int dice2 = random.Next(1, 7);
-                int diceSum = dice1 + dice2;
-
-                rollsCount[diceSum]++;
-            }
-
-            return rollsCount;
-        }
-
 
     }
 }
---
+++ Mission_1/SimulateRollClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateRollClass
+{
+    internal class RollClass
+    {
+        public static int[] SimulateRolls(int numDiceRolls)
+        {
+            int[] rollsCount = new int[13];
+
+            Random random = new Random();
+
+            for (int i = 0; i < numDiceRolls; i++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+                int diceSum = dice1 + dice2;
+
+                rollsCount[diceSum]++;
+            }
+
+            return rollsCount;
+        }
+    }
+}
